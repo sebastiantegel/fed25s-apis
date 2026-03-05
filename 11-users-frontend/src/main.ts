db10b32ce9ea57383282decf05ac78850489015a
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2026-03-05 14:05:29 +0100

    Implemented frontend project for user management

--- 11-users-frontend/src/main.ts
+++ 11-users-frontend/src/main.ts
@@ -1,1 +1,19 @@
+import type { SecretResponse } from "./models/SecretResponse";
 import "./style.css";
+
+// document.querySelector("#getSecret")?.addEventListener("click", () => {});
+document.getElementById("getSecret")?.addEventListener("click", async () => {
+  const response = await fetch("http://localhost:3000/secret", {
+    credentials: "include",
+  });
+
+  if (response.status === 200) {
+    // data -> { message: "The most secret secret of them all "}
+    const data: SecretResponse = await response.json();
+
+    const result = document.getElementById("result");
+    if (result) {
+      result.textContent = data.message;
+    }
+  }
+});

312ea09ecbb6858633d75c7dd57d45422b843d51
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2026-03-18 11:24:23 +0100

    Added project

---
+++ 13-socket-rooms/backend/src/models/Chat.mts
@@ -0,0 +1,6 @@
+import type { Message } from "./message.mjs";
+
+export type Chat = {
+  name: string;
+  messages: Message[];
+};
---
+++ 13-socket-rooms/frontend/src/main.ts
@@ -0,0 +1,95 @@
+import { io } from "socket.io-client";
+import "./style.css";
+import type { Message } from "./models/message";
+
+const socket = io("http://localhost:3000");
+
+let selectedRoom = "";
+let username = "";
+
+document.getElementById("newMessageForm")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const theMessage = (document.getElementById("theMessage") as HTMLInputElement)
+    .value;
+
+  socket.emit(
+    "sendMessage",
+    { message: theMessage, from: username } satisfies Message,
+    selectedRoom,
+  );
+});
+
+socket.on("connect", () => {
+  console.log("Socket:", socket.connected);
+
+  // När ett meddelande har tagits emot och lagrats på servern kommer
+  // vi hit efter io.to("...").emit(...) från servern.
+  socket.on("newMessage", (theMessage: Message) => {
+    // Bygg html baserat på den struktur som Message har
+    createMessageHtml(theMessage);
+  });
+
+  // Här lyssnar vi efter den händelse som ger oss alla rum
+  // i applikationen
+  socket.on("roomList", (rooms: string[]) => {
+    const roomsContainer = document.querySelector("#rooms");
+
+    if (roomsContainer) {
+      // roomsContainer.innerHTML = "";
+
+      // Loopa igenom rummen
+      rooms.forEach((room) => {
+        // Skapa en knapp per tillgängliga rum
+        const roomButton = document.createElement("button");
+        roomButton.textContent = room;
+
+        // Vid klick på en knapp
+        roomButton.addEventListener("click", () => {
+          // Gå med i rummet på servern
+          socket.emit("joinRoom", room);
+
+          // Lagra vårt valda rum så att vi kan skicka meddelanden dit
+          selectedRoom = room;
+
+          // Lagra också användarnamnet så vi vet vem som skickar meddelanden
+          username = (document.getElementById("username") as HTMLInputElement)
+            .value;
+
+          document.getElementById("rooms")?.classList.toggle("hide");
+          document.getElementById("chat-container")?.classList.toggle("hide");
+        });
+
+        roomsContainer.appendChild(roomButton);
+      });
+    }
+  });
+
+  socket.on("chatHistory", (messages: Message[]) => {
+    messages.forEach((message) => {
+      createMessageHtml(message);
+    });
+  });
+});
+
+function createMessageHtml(theMessage: Message) {
+  const chat = document.getElementById("chat");
+  const messageContainer = document.createElement("div");
+
+  const fromTag = document.createElement("span");
+  const messageTag = document.createElement("span");
+
+  fromTag.textContent = theMessage.from + ": ";
+  messageTag.textContent = theMessage.message;
+
+  if (theMessage.from === username) {
+    messageContainer.className = "me";
+  }
+
+  // messageContainer.className = theMessage.from === username ? "me" : "";
+
+  messageContainer?.appendChild(fromTag);
+  messageContainer?.appendChild(messageTag);
+
+  chat?.appendChild(messageContainer);
+}
